aef1c870527d5fb745ba2109caa0f18ea9bf33fe
alirmemon37 <alirmemona@gmail.com>
2023-08-11 22:03:43 +0530

    (chore): auth layout added

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
@@ -9,15 +8,14 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import Container from "@mui/material/Container";
 import { useAuthStore } from "@/store/AuthStore";
 import { useRouter } from "next/navigation";
-import Navbar from "@/components/Navbar";
 import AuthForm from "@/components/Authform/AuthForm";
+import Image from "next/image";
 
 enum provider {
   Google,
   Twitter,
 }
 
-// TODO remove, this demo shouldn't need to reset the theme.
 const LoginPage = () => {
   const router = useRouter();
   const [isLoggedIn, user] = useAuthStore((state) => [state.login, state.user]);
@@ -32,19 +30,38 @@ const LoginPage = () => {
 
   return (
     <>
-    <Navbar onboarding={false} />
-    <AuthForm/>
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Box className="flex justify-center mt-2">
-        <Button className="flex" onClick={() => login(provider.Google)}>
-          <GoogleIcon className="mx-1" />
-        </Button>
-        <Button className="flex" onClick={() => login(provider.Twitter)}>
-          <TwitterIcon className="mx-1" />
-        </Button>
-      </Box>
-    </Container>
+      <div className="left md:w-7/12 w-full relative pt-20 h-full flex flex-col items-center justify-center bg-[#191817]">
+        <div className="md:w-[440px] w-11/12">
+          <h1 className="text-5xl text-center mb-2 font-semibold font-gothic tracking-wide text-[#feec01]">
+            Welcome Back!
+          </h1>
+          <p className="text-base text-center text-[#fefffe]">
+            Login to your account to continue upscaling your career with Mentea
+          </p>
+          <AuthForm />
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box className="flex justify-center mt-2">
+              <Button className="flex" onClick={() => login(provider.Google)}>
+                <GoogleIcon className="mx-1" />
+              </Button>
+              <Button className="flex" onClick={() => login(provider.Twitter)}>
+                <TwitterIcon className="mx-1" />
+              </Button>
+            </Box>
+          </Container>
+        </div>
+      </div>
+      <div className="right md:block hidden w-5/12 h-full">
+        <Image
+          src={"https://picsum.photos/400/600?random=5&grayscale&blur=2"}
+          width="0"
+          height="0"
+          sizes="100vw"
+          className="w-full shadow-2xl opacity-50 h-full object-cover"
+          alt="sidebar image"
+        />
+      </div>
     </>
   );
 };
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,14 +1,34 @@
-import React, { Suspense } from 'react';
-import Navbar from "@/components/Navbar";
-const AuthForm = React.lazy(() => import('@/components/Authform/AuthForm'));
+import React, { Suspense } from "react";
+import Image from "next/image";
+const AuthForm = React.lazy(() => import("@/components/Authform/AuthForm"));
 
 const RegisterPage = () => {
   return (
     <>
-      <Navbar onboarding={false}/>
-      <Suspense fallback={<div>Loading AuthForm...</div>}>
-        <AuthForm isSignUp={true}/>
-      </Suspense>
+      <div className="left md:w-7/12 w-full h-full p-6 flex flex-col items-center justify-center overflow-y-auto bg-[#191817]">
+        <div className="pt-32 flex flex-col items-center">
+          <h1 className="text-5xl text-center mb-2 font-semibold font-gothic tracking-wide text-[#feec01]">
+            Create your Mentea account!
+          </h1>
+          <p className="text-base text-center text-[#fefffe]">
+            Unlock your full potential with Mentea
+          </p>
+
+          <Suspense fallback={<div>Loading AuthForm...</div>}>
+            <AuthForm isSignUp={true} />
+          </Suspense>
+        </div>
+      </div>
+      <div className="right md:block hidden w-5/12 h-full">
+        <Image
+          src={"https://picsum.photos/400/600?random=5&grayscale&blur=2"}
+          width="0"
+          height="0"
+          sizes="100vw"
+          className="w-full shadow-2xl opacity-50 h-full object-cover"
+          alt="sidebar image"
+        />
+      </div>
     </>
   );
 };
