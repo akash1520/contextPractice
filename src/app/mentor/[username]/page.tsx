934b4bbb14e7d30e931dd776afc8bd6da97fcb0a
alirmemon37 <alirmemona@gmail.com>
2023-09-02 16:30:11 +0530

    (chore): dynamic mentor pages

---
+++ src/app/mentor/[username]/page.tsx
@@ -0,0 +1,57 @@
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { db } from "@/firebase";
+import MentorView from "./components/MentorView";
+
+interface MentorPageProps {
+  params: {
+    username: string;
+  };
+}
+
+export async function generateMetadata({
+  params,
+}: MentorPageProps): Promise<Metadata> {
+  // read route params
+  const { username } = params;
+  const mentor = await getMentorData(username);
+
+  if (!mentor) {
+    return {
+      title: "Mentor Not Found",
+    };
+  }
+
+  return {
+    title: `Book a session with ${mentor.firstName} ${mentor.lastName} @ Mentea`,
+  };
+}
+
+const getMentorData = async (username: string) => {
+  const mentorsCollectionRef = collection(db, "Mentors");
+  const q = query(mentorsCollectionRef, where("username", "==", username));
+  const querySnapshot = await getDocs(q);
+
+  if (querySnapshot.empty) {
+    return undefined;
+  }
+
+  const mentor = querySnapshot.docs[0].data() as Mentor;
+
+  return mentor;
+};
+
+const MentorPage = async ({ params }: MentorPageProps) => {
+  const { username } = params;
+
+  const mentor = await getMentorData(username);
+
+  if (!mentor) {
+    notFound();
+  }
+
+  return <MentorView mentor={mentor} />;
+};
+
+export default MentorPage;
---
+++ src/types/mentor.d.ts
@@ -0,0 +1,14 @@
+interface Mentor {
+  firstName: string;
+  lastName: string;
+  username: string;
+  shortHeading: string;
+  gender: string;
+  age: string;
+  about?: string;
+  organization: string;
+  role: string;
+  experience: number;
+  languages: string[];
+  socials: string[];
+}
